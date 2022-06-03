50707b0bff5b417d3b51649ea603a8756ca9b351
Francisco Lacerda <flacerda@vasp.pt>
2022-06-03 23:10:04 +0100

    persistence registration and git ignore

---
+++ NSRP/src/NonSuckingRepositoryPattern.Solution/NSRP.Persistence/PersistenceServicesRegistration.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace NSRP.Persistence
+{
+    public static class PersistenceServicesRegistration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            return services;
+        }
+    }
+}
